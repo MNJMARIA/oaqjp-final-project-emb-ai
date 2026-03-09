ff29bb3a1e367c4d6e26506070fc853007686f23
MNJMARIA <stepanovamasha70@gmail.com>
2026-03-09 18:13:08 -0400

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,17 +2,24 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    if not text_to_analyze.strip():  # якщо порожній рядок або лише пробіли
+        return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = header)
-    formatted_response = json.loads(response.text)
-    anger = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
-    disgust = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
-    fear = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
-    joy = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
-    sadness = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
-    emotions = {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness}
-    dominant_emotion = max(emotions, key = emotions.get)
 
-    return {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness, "dominant_emotion": dominant_emotion}
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        anger = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+        disgust = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+        fear = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+        joy = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+        sadness = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+        emotions = {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness}
+        dominant_emotion = max(emotions, key = emotions.get)
+        return {"anger": anger, "disgust": disgust, "fear": fear, "joy": joy, "sadness": sadness, "dominant_emotion": dominant_emotion}
+    else:
+        return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+    
--- server.py
+++ server.py
@@ -12,15 +12,15 @@ def send_detector():
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
 
-    if not text_to_analyze:
-        return "There is no text_to_analyze"
-
     anger = result["anger"]
     disgust = result["disgust"]
     fear = result["fear"]
     joy = result["joy"]
     sadness = result["sadness"]
     dominant_emotion = result["dominant_emotion"]
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
 
     return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
